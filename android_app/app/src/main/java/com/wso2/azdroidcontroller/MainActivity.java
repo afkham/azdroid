bef7e7782bfbbb0fe917cf47d9c6ea2c6b8da991
Afkham Azeez <azeez@wso2.com>
2015-06-15 17:31:37 +0530

    Added images

--- android_app/app/src/main/java/com/wso2/azdroidcontroller/MainActivity.java
+++ android_app/app/src/main/java/com/wso2/azdroidcontroller/MainActivity.java
@@ -1,17 +1,66 @@
 package com.wso2.azdroidcontroller;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonForward).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Moving forward...", Toast.LENGTH_SHORT).show();
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpGet request = new HttpGet("http://foo.com");
+            }
+        });
+        findViewById(R.id.buttonReverse).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Moving backward...", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        findViewById(R.id.buttonLeft).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Turning left...", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        findViewById(R.id.buttonRight).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Turning right...", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        findViewById(R.id.buttonStop).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Stopping...", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
 
